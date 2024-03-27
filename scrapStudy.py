a7140ad92a4e93f4e88287c935c79fc080e32db5
Coutin-05 <120342150+Coutin-05@users.noreply.github.com>
2024-03-27 18:15:25 +0000

    Add knowledge check for the find text feature using the find method

--- scrapStudy.py
+++ scrapStudy.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 import re
 
-url = "http://olympus.realpython.org/profiles/poseidon"
+'''url = "http://olympus.realpython.org/profiles/poseidon"
 page = urlopen(url)
 htmlBytes = page.read()
 html = htmlBytes.decode("utf-8")
@@ -10,4 +10,33 @@ startIndex = titleIndex + len("<title>")
 endIndex = html.find("</title>")
 title = html[startIndex:endIndex]
 find = re.findall("ab*c", "ABC", re.IGNORECASE)
-print(find)
+print(find)'''
+
+'''url = "http://olympus.realpython.org/profiles/dionysus"
+page = urlopen(url)
+# this method unifies the two variables above, htmlBytes, html in a single line
+html = page.read().decode("utf-8")
+
+pattern = "<title.*?>.*?</title.*?>"
+matchResults = re.search(pattern, html, re.IGNORECASE)
+title = matchResults.group()
+title = re.sub("<.*?>", "", title) #  remove HTML tags
+
+print(title)'''
+
+url = "http://olympus.realpython.org/profiles/dionysus"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+
+for string in ["Name: ", "Favorite Color:"]:
+    stringStartIdx = html.find(string)
+    textStartIdx = stringStartIdx +len(string)
+
+    nextHTMLtagOffset = html[textStartIdx:].find("<")
+    textEndIdx = textStartIdx + nextHTMLtagOffset
+
+    rawText = html[textStartIdx : textEndIdx]
+    cleanText = rawText.strip(" \r\n\t")
+    print(cleanText)
+    print(textStartIdx)
+    print(textEndIdx)
